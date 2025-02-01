b42f96ee5b111ba7baa46c3d049615cf8b4e8096
giangallino <99291206+gianlucaegallino@users.noreply.github.com>
2025-02-01 15:12:38 -0300

    Add main functionality

--- app.js
+++ app.js
@@ -2,19 +2,16 @@ require("dotenv").config();
 const path = require("node:path");
 const express = require("express");
 const indexRouter = require("./routes/indexRouter");
-const newRouter = require("./routes/newRouter");
 const assetsPath = path.join(__dirname, "public");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-
 app.use("/", indexRouter);
-app.use("/new", newRouter);
-
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,18 +4,30 @@ const indexRouter = Router();
 
 //sample messages
 const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ];
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().toLocaleString("en-GB"),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toLocaleString("en-GB"),
+  },
+];
 
-indexRouter.get("/", (req, res) => res.render("index", {title: "NeuChat",messages:messages}));
+indexRouter.get("/", (req, res) =>
+  res.render("index", { title: "Messages", messages: messages })
+);
+
+indexRouter.get("/new", (req, res) => res.render("form", {}));
+
+indexRouter.post("/new", (req, res) => {
+  let name = req.body.name;
+  let text = req.body.text;
+
+  messages.push({ text: text, user:name, added: new Date().toLocaleString("en-GB") });
+  res.redirect("/");
+});
 
 module.exports = indexRouter;
